ed5ce6fdd9d288760cbf1a53118d51b0347693b5
Morgan Schneider <me.schneider0108@gmail.com>
2018-05-31 16:57:42 -0600

    Update
    Added additional grouping of data by day (based on ~15 orbits per day), calculated the daily mean and standard deviation for F2 values more than 15 deg from the equator, made a few random plots for code purposes.
    
    Goals: develop algorithm to detect peaks in intensity using the mean and standard deviation of F2 and mapping to F1.

--- find_EEJ.m
+++ find_EEJ.m
@@ -1,8 +1,18 @@
 %% This will eventually be a script for finding the location of the EEJ
 
+% Parameters:
+%
+% Time (seconds since Jan 1, 1970 - epoch time)
+% Radius (meters from the center of the earth)
+% Longitude (degrees)
+% Geocentric latitude (degrees)
+% Quasi-dipole latitude (degrees)
+% Field intensity (nT)
+% Field intensity corrected for Sq (nT)
+
 load('./EEJ_Data/Swarm_Data.mat')
 
-%% Find the starting indices of each separate orbit
+%% Find the starting indices of each individual orbit
 
 k = 1;
 A_inds = zeros(1,10000);
@@ -40,12 +50,29 @@ for i = 1:length(swarm(3).time) - 1
 end
 nOrbits_C = k;
 
-%% Find the starting indices of each individual orbit
+% Vectors of the starting indices of each orbit
 A_inds = [A_inds(A_inds ~= 0) length(swarm(1).time)+1];
 B_inds = [B_inds(B_inds ~= 0) length(swarm(2).time)+1];
 C_inds = [C_inds(C_inds ~= 0) length(swarm(3).time)+1];
 
 %% Separate orbits
+
+% A.orbit: 2505 orbits, 167 days
+% B.orbit: 19773 orbits, 1318 days
+% C.orbit: 19913 orbits, 1327 days
+%
+% Swarm A time range: 20131126 00:38:00 - 20140522 11:36:35 (why only 6 months for this one?)
+% Swarm B time range: 20131128 00:00:00 - 20170715 22:48:04
+% Swarm C time range: 20131204 00:59:41 - 20170715 23:47:18
+%
+% One orbit is ~33 minutes
+% ~1 hour and 3 min between orbits
+% One orbit "cycle" is ~1 hour and 35 minutes
+% ~15 orbits per day
+
+nDays_A = floor(nOrbits_A / 15);
+nDays_B = floor(nOrbits_B / 15);
+nDays_C = floor(nOrbits_C / 15);
 
 j = 1;
 for i = 1:nOrbits_A
@@ -92,7 +119,149 @@ for i = 1:nOrbits_C
     j = C_inds(i);
 end
 
+%% Separate days
 
+for i = 1:nDays_A
+    A.day(i).time = [];
+    A.day(i).rad = [];
+    A.day(i).lon = [];
+    A.day(i).geolat = [];
+    A.day(i).qdlat = [];
+    A.day(i).F1 = [];
+    A.day(i).F2 = [];
+    for j = 1:15
+        ind = (i-1) * 15 + j;
+        A.day(i).time = [A.day(i).tifigureme, A.orbit(ind).time];
+        A.day(i).rad = [A.day(i).rad, A.orbit(ind).rad];
+        A.day(i).lon = [A.day(i).lon, A.orbit(ind).lon];
+        A.day(i).geolat = [A.day(i).geolat, A.orbit(ind).geolat];
+        A.day(i).qdlat = [A.day(i).qdlat, A.orbit(ind).qdlat];
+        A.day(i).F1 = [A.day(i).F1, A.orbit(ind).F1];
+        A.day(i).F2 = [A.day(i).F2, A.orbit(ind).F2];
+    end
+end
+
+for i = 1:nDays_B
+    B.day(i).time = [];
+    B.day(i).rad = [];
+    B.day(i).lon = [];
+    B.day(i).geolat = [];
+    B.day(i).qdlat = [];
+    B.day(i).F1 = [];
+    B.day(i).F2 = [];
+    for j = 1:15
+        ind = (i-1) * 15 + j;
+        B.day(i).time = [B.day(i).time, B.orbit(ind).time];
+        B.day(i).rad = [B.day(i).rad, B.orbit(ind).rad];
+        B.day(i).lon = [B.day(i).lon, B.orbit(ind).lon];
+        B.day(i).geolat = [B.day(i).geolat, B.orbit(ind).geolat];
+        B.day(i).qdlat = [B.day(i).qdlat, B.orbit(ind).qdlat];
+        B.day(i).F1 = [B.day(i).F1, B.orbit(ind).F1];
+        B.day(i).F2 = [B.day(i).F2, B.orbit(ind).F2];
+    end
+end
+
+for i = 1:nDays_C
+    C.day(i).time = [];
+    C.day(i).rad = [];
+    C.day(i).lon = [];
+    C.day(i).geolat = [];
+    C.day(i).qdlat = [];
+    C.day(i).F1 = [];
+    C.day(i).F2 = [];
+    for j = 1:15
+        ind = (i-1) * 15 + j;
+        C.day(i).time = [C.day(i).time, C.orbit(ind).time];
+        C.day(i).rad = [C.day(i).rad, C.orbit(ind).rad];
+        C.day(i).lon = [C.day(i).lon, C.orbit(ind).lon];
+        C.day(i).geolat = [C.day(i).geolat, C.orbit(ind).geolat];
+        C.day(i).qdlat = [C.day(i).qdlat, C.orbit(ind).qdlat];
+        C.day(i).F1 = [C.day(i).F1, C.orbit(ind).F1];
+        C.day(i).F2 = [C.day(i).F2, C.orbit(ind).F2];
+    end
+end
+
+
+%% Some time bullshit
+
+time_unix_A = zeros(1, nOrbits_A);
+for i = 1:nOrbits_A
+    time_unix_A(i) = A.orbit(i).time(1);
+end
+time_unix_B = zeros(1, nOrbits_B);
+for i = 1:nOrbits_B
+    time_unix_B(i) = B.orbit(i).time(1);
+end
+time_unix_C = zeros(1, nOrbits_C);
+for i = 1:nOrbits_C
+    time_unix_C(i) = C.orbit(i).time(1);
+end
+
+time_str_A = datestr((datenum('1970', 'yyyy') + time_unix_A ./ 8.64e4), 'yyyymmdd HH:MM:SS');
+time_str_B = datestr((datenum('1970', 'yyyy') + time_unix_B ./ 8.64e4), 'yyyymmdd HH:MM:SS');
+time_str_C = datestr((datenum('1970', 'yyyy') + time_unix_C ./ 8.64e4), 'yyyymmdd HH:MM:SS');
+
+for i = 1:nOrbits_A
+    A.orbit(i).start = time_str_A(i,:);
+end
+for i = 1:nOrbits_B
+    B.orbit(i).start = time_str_B(i,:);
+end
+for i = 1:nOrbits_C
+    C.orbit(i).start = time_str_C(i,:);
+end
+
+
+%% Some plots or something
+
+figure(1)
+subplot(2, 1, 1)
+hold on
+for i = 1:15
+    title('Swarm A 11-26-2013')
+    plot(A.orbit(i).geolat, A.orbit(i).F1)
+    if i <= 5
+        title('F1')
+    end
+end
+hold off
+title('F1, 11-26-2013')
+subplot(2, 1, 2)
+hold on
+for i = 1:15
+    plot(A.orbit(i).geolat, A.orbit(i).F2)
+end
+hold off
+title('F2, 11-26-2013')
+
+
+
+figure(2)
+hold on
+for i = 1:15
+    plot(A.day(i).geolat, A.day(i).F2, '*r')
+    plot(B.day(i).geolat, B.day(i).F2, '*b')
+    plot(C.day(i).geolat, C.day(i).F2, '*g')
+end
+hold off
+
+%% Some more junk
+
+
+for i = 1:nDays_A
+    A.day(i).meanF2 = mean([A.day(i).F2(A.day(i).geolat >= 15), A.day(i).F2(A.day(i).geolat <= -15)]);
+    A.day(i).stdvF2 = std([A.day(i).F2(A.day(i).geolat >= 15), A.day(i).F2(A.day(i).geolat <= -15)]);
+end
+
+for i = 1:nDays_B
+    B.day(i).meanF2 = mean([B.day(i).F2(B.day(i).geolat >= 15), B.day(i).F2(B.day(i).geolat <= -15)]);
+    B.day(i).stdvF2 = std([B.day(i).F2(B.day(i).geolat >= 15), B.day(i).F2(B.day(i).geolat <= -15)]);
+end
+
+for i = 1:nDays_C
+    C.day(i).meanF2 = mean([C.day(i).F2(C.day(i).geolat >= 15), C.day(i).F2(C.day(i).geolat <= -15)]);
+    C.day(i).stdvF2 = std([C.day(i).F2(C.day(i).geolat >= 15), C.day(i).F2(C.day(i).geolat <= -15)]);
+end
 
 
 
