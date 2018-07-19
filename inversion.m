77337a59e10aaaf9c961e286d4027c6199e35346
Morgan Schneider <me.schneider0108@gmail.com>
2018-07-19 17:40:54 -0600

    Add files via upload

--- inversion.m
+++ inversion.m
@@ -12,9 +12,9 @@ pp_N.coefs = reshape(coefs_tmp(1:N*(N+2),:,:), [], pp.order);
 
 %% Retrieve EEJ peaks
 
-[t1, lat1, lon1, r1, ~, ~, ~] = find_EEJ(swarm, 1);
-[t2, lat2, lon2, r2, ~, ~, ~] = find_EEJ(swarm, 2);
-[t3, lat3, lon3, r3, ~, ~, ~] = find_EEJ(swarm, 3);
+[t1, lat1, lon1, r1, qd1, ~, ~] = find_EEJ(swarm, 1);
+[t2, lat2, lon2, r2, qd2, ~, ~] = find_EEJ(swarm, 2);
+[t3, lat3, lon3, r3, qd3, ~, ~] = find_EEJ(swarm, 3);
 
 %% Separate times
 t_EEJ = [t1, t2, t3];
@@ -46,25 +46,29 @@ r_s = [scalar(1).rad, scalar(2).rad]; r_s = r_s(inds_s);
 theta_s = (90 - [scalar(1).geolat, scalar(2).geolat]) * rad; theta_s = theta_s(inds_s);
 phi_s = [scalar(1).lon, scalar(2).lon] * rad; phi_s = phi_s(inds_s);
 
-Br_EEJ = zeros(length(r_r), 1);
+qd_r = [qd1, qd2, qd3]; qd_r = qd_r(inds_r);
+qd_s = [scalar(1).qdlat, scalar(2).qdlat]; qd_s = qd_s(inds_s);
+
+B_EEJ = synth_values(r_r, theta_r./rad, phi_r./rad, pp_N, mjd_r);
+Br_EEJ = B_EEJ(:,1);
 F_swarm = rot90([scalar(1).F, scalar(2).F], 3); F_swarm = F_swarm(inds_s);
 
 %% Invert
 
-g_init = ones(NSH,1);
+g_init = zeros(NSH,1);
 g_init(index(1,0)) = -30000; % Earth's dipole moment is roughly 30000 nT
 gamma = 0.5;
 [J_alpha,~] = find_J(r_r, theta_r, phi_r, g_init, N);
 
 % Build weighting functions
-W_r = diag(ones(1,length(r_r)) * 30);
+W_r = diag(ones(1,length(r_r)) * 10);
 W_s = zeros(length(r_s), length(r_s));
 for i = 1:length(r_s)
     W_s(i,i) = sin(theta_s(i));
 end
 
 
-for x = 1:30
+for x = 1:50
     gprev = g_init;
     alpha = find_alpha(Br_EEJ, r_r, theta_r, phi_r, g_init, N);
     beta = find_beta(F_swarm, r_s, theta_s, phi_s, g_init, N);
@@ -92,6 +96,125 @@ F_model = find_F(r_s, theta_s, phi_s, g_model, N);
 % compare to CHAOS
 B_chaos = synth_values(r_r, theta_r, phi_r, pp_N, mjd_r);
 F_chaos = find_F(r_s, theta_s, phi_s, pp_N, mjd_s);
+
+
+%% Plot
+subplot(2,1,1)
+plot(lon_deg_r, Br_EEJ - B_model(:,1), '*')
+subplot(2,1,2)
+plot(qd_s, F_swarm - F_model, '*')
+
+%%
+
+Br = B_model(:,1);
+Bt = B_model(:,2);
+Bp = B_model(:,3);
+deltaBr = B_model(:,1) - B_chaos(:,1);
+deltaBt = B_model(:,2) - B_chaos(:,2);
+deltaBp = B_model(:,3) - B_chaos(:,3);
+deltaF = F_model - F_chaos;
+lat_deg = 90 - (theta_s./rad);
+lon_deg = phi_s./rad;
+lon_deg_r = phi_r./rad;
+%%
+figure(1)
+subplot(2,1,1)
+plot(lon_deg, B_model(:,1) - B_chaos(:,1), '*')
+xlabel('Longitude (deg)')
+ylabel('\alpha (nT)')
+title('B_{r} Model Residuals')
+subplot(2,1,2)
+plot(qd_s, F_model - F_chaos, '*')
+xlabel('Quasi-Dipole Latitude (deg)')
+ylabel('\beta (nT)')
+title('F Model Residuals')
+
+lat_rng = [-90 90];
+lon_rng = [-180 180];
+
+lat_lin = linspace(min(lat_deg), max(lat_deg), 2000);
+lon_lin = linspace(min(lon_deg), max(lon_deg), 2000);
+[lat_grid, lon_grid] = meshgrid(lat_lin, lon_lin);
+Br_grid = griddata(lat_deg, lon_deg, Br, lat_grid, lon_grid, 'cubic');
+Bt_grid = griddata(lat_deg, lon_deg, Bt, lat_grid, lon_grid, 'cubic');
+Bp_grid = griddata(lat_deg, lon_deg, Bp, lat_grid, lon_grid, 'cubic');
+F_grid = griddata(lat_deg, lon_deg, F_model, lat_grid, lon_grid, 'cubic');
+dBr_grid = griddata(lat_deg, lon_deg, deltaBr, lat_grid, lon_grid, 'cubic');
+dBt_grid = griddata(lat_deg, lon_deg, deltaBt, lat_grid, lon_grid, 'cubic');
+dBp_grid = griddata(lat_deg, lon_deg, deltaBp, lat_grid, lon_grid, 'cubic');
+dF_grid = griddata(lat_deg, lon_deg, deltaF, lat_grid, lon_grid, 'cubic');
+
+% why
+figure(2)
+load coastlines
+
+subplot(2,2,1)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, Br_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('B_r (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,2)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, Bt_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('B_{\theta} (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,3)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, Bp_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('B_{\phi} (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,4)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, F_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('F (nT)')
+contourcbar('southoutside')
+
+
+figure(3)
+
+subplot(2,2,1)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, dBr_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('\delta B_r (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,2)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, dBt_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('\delta B_{\theta} (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,3)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, dBp_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('\delta B_{\phi} (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,4)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, dF_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('\delta F (nT)')
+contourcbar('southoutside')
+
 
 
 
--- model_scalar.m
+++ model_scalar.m
@@ -1,23 +1,31 @@
 %
-
+load('./EEJ_Data/Swarm_Scalar.mat')
+%%
 rad = pi/180; %radians
 
-r = ones(1,1000) * 6500; %km
-theta = rand(1000,1) * 180; theta = theta * rad; %radians
-phi = rand(1000,1) * 360 - 180; phi = phi * rad; %radians
+t_scalar = [scalar(1).time, scalar(2).time];
+yearEEJ = decimalYear(t_EEJ);
+yearScalar = [scalar(1).year, scalar(2).year];
+
+timeStart = 2015.0;
+timeEnd = 2015.25;
+
+inds = find(yearScalar >= timeStart & yearScalar < timeEnd);
+t = t_scalar(inds);
+mjd = datenum(datetime(t,'ConvertFrom','posixtime')) - datenum(2000,1,1,0,0,0);
+
+r = [scalar(1).rad, scalar(2).rad]; r = r(inds);
+theta = (90 - [scalar(1).geolat, scalar(2).geolat]) * rad; theta = theta(inds);
+phi = [scalar(1).lon, scalar(2).lon] * rad; phi = phi(inds);
 N = 10;
 
 NSH = (N+1)^2-1;
-g_synth = rand(NSH,1);
-g_synth(index(1,0)) = 1;
 
-F_synth = find_F(r, theta, phi, g_synth, N);
+F_synth = rot90([scalar(1).F, scalar(2).F], 3); F_synth = F_synth(inds);
 
 %%
 
-g_init = rand(NSH,1);
-% W = weighting function;
-gamma = 0.5;
+g_init = ones(NSH,1);
 
 for x = 1:50
     gprev = g_init;
@@ -26,6 +34,131 @@ for x = 1:50
     delta = J\beta;
     g_init = g_init + gamma*delta;
     error = norm(g_init - gprev) / norm(g_init)
-    total_error = norm(g_synth - g_init) / norm(g_synth)
 end
 %final_error = norm(g_synth - g_init) / norm(g_synth)
+
+%%
+
+B_scalar_model = find_B(r, theta, phi, g_init, N);
+F_scalar_model = find_F(r, theta, phi, g_init, N);
+
+% compare to CHAOS
+B_chaos = synth_values(r, theta, phi, pp_N, mjd);
+F_chaos = find_F(r, theta, phi, pp_N, mjd);
+
+
+
+%%
+
+Br = B_scalar_model(:,1);
+Bt = B_scalar_model(:,2);
+Bp = B_scalar_model(:,3);
+deltaBr = B_scalar_model(:,1) - B_chaos(:,1);
+deltaBt = B_scalar_model(:,2) - B_chaos(:,2);
+deltaBp = B_scalar_model(:,3) - B_chaos(:,3);
+deltaF = F_scalar_model - F_chaos;
+lat_deg = 90 - (theta_s./rad);
+lon_deg = phi_s./rad;
+lon_deg_r = phi_r./rad;
+
+figure(1)
+subplot(2,1,1)
+plot(lon_deg, B_scalar_model(:,1) - B_model(:,1), '*')
+xlabel('Longitude (deg)')
+ylabel('\alpha (nT)')
+title('B_{r} Model Residuals')
+subplot(2,1,2)
+plot(qd_s, F_scalar_model - F_model, '*')
+xlabel('Quasi-Dipole Latitude (deg)')
+ylabel('\beta (nT)')
+title('F Model Residuals')
+
+lat_rng = [-90 90];
+lon_rng = [-180 180];
+
+lat_lin = linspace(min(lat_deg), max(lat_deg), 2000);
+lon_lin = linspace(min(lon_deg), max(lon_deg), 2000);
+[lat_grid, lon_grid] = meshgrid(lat_lin, lon_lin);
+Br_grid = griddata(lat_deg, lon_deg, Br, lat_grid, lon_grid, 'cubic');
+Bt_grid = griddata(lat_deg, lon_deg, Bt, lat_grid, lon_grid, 'cubic');
+Bp_grid = griddata(lat_deg, lon_deg, Bp, lat_grid, lon_grid, 'cubic');
+F_grid = griddata(lat_deg, lon_deg, F_scalar_model, lat_grid, lon_grid, 'cubic');
+dBr_grid = griddata(lat_deg, lon_deg, deltaBr, lat_grid, lon_grid, 'cubic');
+dBt_grid = griddata(lat_deg, lon_deg, deltaBt, lat_grid, lon_grid, 'cubic');
+dBp_grid = griddata(lat_deg, lon_deg, deltaBp, lat_grid, lon_grid, 'cubic');
+dF_grid = griddata(lat_deg, lon_deg, deltaF, lat_grid, lon_grid, 'cubic');
+
+% why
+figure(2)
+load coastlines
+
+subplot(2,2,1)
+
+ax = worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, Br_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('B_r (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,2)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, Bt_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('B_{\theta} (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,3)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, Bp_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('B_{\phi} (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,4)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, F_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('F (nT)')
+contourcbar('southoutside')
+
+
+figure(3)
+
+subplot(2,2,1)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, dBr_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('\delta B_r (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,2)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, dBt_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('\delta B_{\theta} (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,3)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, dBp_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('\delta B_{\phi} (nT)')
+contourcbar('southoutside')
+
+subplot(2,2,4)
+
+worldmap(lat_rng, lon_rng);
+contourfm(lat_grid, lon_grid, dF_grid)
+plotm(coastlat, coastlon, 'Color', 'black')
+title('\delta F (nT)')
+contourcbar('southoutside')
+
+
+
+
